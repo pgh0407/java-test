4230ccfccda40ef41fbc35ea6cf3c305eb368927
pgh0407 <qpwee123@gmail.com>
2025-07-11 13:59:13 +0900

    calculator2

--- Calculate.java
+++ Calculate.java
@@ -2,35 +2,33 @@ package org.example;
 
 public class Calculate {
 
-    public static void calculate(int num1, int num2, char operator) {
-        switch(operator) {
-            case '+':
-                System.out.println("답: " + (num1 + num2));
-                break;
-            case '-':
-                System.out.println("답: " + (num1 - num2));
-                break;
-            case '*':
-                System.out.println("답: " + (num1 * num2));
-                break;
-            case '/':
-                if (num2 != 0) {
-                    System.out.println("답: " + (num1 / num2));
-                } else {
-                    System.out.println("Error");
-                }
-                break;
-            default:
-                System.out.println("빈 칸");
-                break;
-        }
-    }
-
     public static void main(String[] args) {
         int num1 = 15;
         int num2 = 3;
-        char operator = '/';
 
-        calculate(num1, num2, operator);
+        System.out.println("더하기: " + add(num1, num2));
+        System.out.println("빼기: " + subtract(num1, num2));
+        System.out.println("곱하기: " + multiply(num1, num2));
+        System.out.println("나누기: " + divide(num1, num2));
+    }
+
+    public static int add(int a, int b) {
+        return a + b;
+    }
+
+    public static int subtract(int a, int b) {
+        return a - b;
+    }
+
+    public static int multiply(int a, int b) {
+        return a * b;
+    }
+
+    public static int divide(int a, int b) {
+        if (b == 0) {
+            System.out.println("ERROR");
+            return 0;
+        }
+        return a / b;
     }
 }
